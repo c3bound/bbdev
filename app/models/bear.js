d38814a19fc61c4966f40114c4478240c933c237
Dustin LeBleu <dustin@mathenomics.com>
2016-11-14 00:31:06 -0600

    t

--- app/models/bear.js
+++ app/models/bear.js
@@ -1,8 +1,15 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var BearSchema   = new Schema({
-	Symbol: String
+var BearSchema   = Schema({
+	_id : String,
+    Symbol : String,
+    CreatedDttm : String,
+    MessageType : String,
+    DateTime : String,
+    PriceAtAlert : String,
+    Message : String,
+    Exchange : String
 });
 
 module.exports = mongoose.model('AlertStreamNasdaq', BearSchema);
